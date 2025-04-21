857da0c762e94e4dbd5336026e067b5a42fc2cf7
geoffreykemboi <geoffreyk126@gmail.com>
2025-04-22 01:18:48 +0300

    propscardstest

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from "./Card";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My Contacts</h1>
+      <Card
+        name="Beyonce"
+        image="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        tel="+123 456 789"
+        email="b@beyonce.com"
+      />
+      <Card
+        name="Jack Bauer"
+        image="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+        tel="+987 654 321"
+        email="jack@nowhere.com"
+      />
+      <Card
+        name="Chuck Norris"
+        image="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+        tel="+918 372 574"
+        email="gmail@chucknorris.com"
+      />
     </div>
   );
 }
---
+++ src/Card.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.image} alt="avatar" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
+export default Card;
